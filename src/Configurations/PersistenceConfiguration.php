b927da698fb6167f4049865ab8a3340ae2ed1a4a
Christian Ramelow <info@codekandis.net>
2021-10-18 20:58:15 +0200

    Adds the persistence configuration.

---
+++ src/Configurations/PersistenceConfiguration.php
@@ -0,0 +1,69 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphyPersistenceIntegration\Configurations;
+
+use CodeKandis\Configurations\AbstractConfiguration;
+use CodeKandis\Persistence\Configurations\PersistenceConfigurationInterface;
+
+/**
+ * Represents a persistence configuration.
+ * @package codekandis/tiphy-persistence-integration
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class PersistenceConfiguration extends AbstractConfiguration implements PersistenceConfigurationInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getDriver(): string
+	{
+		return $this->read( 'driver' );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getHost(): string
+	{
+		return $this->read( 'host' );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getPort(): ?int
+	{
+		return $this->readOrDefault( 'port', null );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getDatabaseName(): string
+	{
+		return $this->read( 'databaseName' );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getUsername(): string
+	{
+		return $this->read( 'username' );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getPassphrase(): string
+	{
+		return $this->read( 'passphrase' );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getAttributes(): array
+	{
+		return $this->readOrDefault( 'attributes', [] );
+	}
+}
